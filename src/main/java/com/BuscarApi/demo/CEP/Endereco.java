da9868d05f3597fdcdeb1a87e0a6b4b4d35b02b2
marcoshaag <marcoshaag2019@gmail.com>
2024-09-05 21:48:46 -0300

    BUSCAR CEP

--- src/main/java/com/BuscarApi/demo/CEP/Endereco.java
+++ src/main/java/com/BuscarApi/demo/CEP/Endereco.java
@@ -1,4 +1,6 @@
 package com.BuscarApi.demo.CEP;
 
-public record Endereco() {
+public record Endereco(String cep, String logradouro,
+                       String complemento, String bairro,
+                       String localidade, String uf) {
 }
--- src/main/java/com/BuscarApi/demo/DemoApplication.java
+++ src/main/java/com/BuscarApi/demo/DemoApplication.java
@@ -1,13 +1,31 @@
 package com.BuscarApi.demo;
 
+import com.BuscarApi.demo.CEP.ConsultaCep;
+import com.BuscarApi.demo.CEP.Endereco;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
 
+		Scanner ler = new Scanner(System.in);
+		ConsultaCep consultaCep = new ConsultaCep();
+
+		System.out.println("Digite o CEP: ");
+		var cep = ler.nextLine();
+
+		try {
+			Endereco endereco = consultaCep.buscarEndereco(cep);
+			System.out.println(endereco.bairro());
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+			System.out.println("Erro");
+		}
+
+
+	}
 }
