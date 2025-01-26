5673b6deaa123a5f6c473bb24ec28bc7e8b98a9d
marcoshaag <marcoshaag2019@gmail.com>
2025-01-26 14:55:29 -0300

    Ajuste method Buscar Cep

---
+++ src/main/java/com/BuscarApi/demo/CEP/ConsultaCep.java
@@ -0,0 +1,38 @@
+package com.BuscarApi.demo.CEP;
+
+
+import com.google.gson.Gson;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Service
+public class ConsultaCep {
+
+    private static final String BASE_URL = "https://viacep.com.br/ws/";
+
+
+    public Endereco buscarEndereco(String cep) {
+
+        String url = BASE_URL + cep + "/json/";
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+
+        try {
+            HttpResponse<String> response = HttpClient.newHttpClient()
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+            return new Gson().fromJson(response.body(), Endereco.class);
+        } catch (IOException | InterruptedException e ) {
+            throw new RuntimeException("Ero ao adicionar cep" + e);
+        }
+
+
+
+    }
+}
